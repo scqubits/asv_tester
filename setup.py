7a468c88886e0c659bc51053cfc52d05ce98d2d3
yellowbow <kchen4000@gmail.com>
2023-07-21 16:04:06 -0500

    updated .gitignore, asv.conf.json, and setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='asv_tester',
     version='0.1.0',
-    packages=find_packages(include=['asv_tester', 'asv_tester.*'])
+    packages=find_packages(include=['asv_tester'])
 )
