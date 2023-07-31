5714f9eb16736e0738acd5deec752dffa75a2487
yellowbow <kchen4000@gmail.com>
2023-07-31 13:30:22 -0500

    changed time sleep from 2 to 4

--- asv_tester/bar.py
+++ asv_tester/bar.py
@@ -4,5 +4,5 @@ def hello():
     return "hello"
 
 def slow_function():
-    time.sleep(2)    # consider changing this line to simulate a code change that alters overall runtime
+    time.sleep(1)    # consider changing this line to simulate a code change that alters overall runtime
 
